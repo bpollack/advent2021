a4ffd0209d8b1754ad74ddc3b7b8dde90237680e
Benjamin Pollack <benjamin@bitquabit.com>
2021-12-01 14:33:28 -0500

    moar cleanup

--- deno/1.ts
+++ deno/1.ts
@@ -1,7 +1,11 @@
-import { zip } from "./deps.ts";
-const nums = Deno.readTextFileSync("../data/1.txt").split("\n").map((n) =>
-  parseInt(n, 10)
+import { path, slidingWindows } from "./deps.ts";
+const root = path.dirname(
+  Deno.build.os === "windows"
+    ? path.win32.fromFileUrl(import.meta.url)
+    : path.posix.fromFileUrl(import.meta.url),
 );
+const nums = Deno.readTextFileSync(path.join(root, "..", "data", "1.txt"))
+  .split("\n").map((n) => parseInt(n, 10));
 let jumps = 0;
 let last = nums[0];
 for (const num of nums) {
@@ -14,7 +18,7 @@ console.log(`First part: ${jumps}`);
 
 jumps = 0;
 last = nums[0] + nums[1] + nums[2];
-for (const triplet of zip(nums, nums.slice(1), nums.slice(2))) {
+for (const triplet of slidingWindows(nums, 3)) {
   const sum = triplet[0] + triplet[1] + triplet[2];
   if (sum > last) {
     jumps++;
--- deno/deps.ts
+++ deno/deps.ts
@@ -1,1 +1,6 @@
-export { zip } from "https://deno.land/std@0.116.0/collections/mod.ts";
+import * as path from "https://deno.land/std@0.116.0/path/mod.ts";
+export {
+  slidingWindows,
+  zip,
+} from "https://deno.land/std@0.116.0/collections/mod.ts";
+export { path };

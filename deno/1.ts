7df106d39187f626719e23d6f3a12ed76be0645e
Benjamin Pollack <benjamin@bitquabit.com>
2021-12-02 09:31:55 -0500

    make a common file for reused bits

--- deno/1.ts
+++ deno/1.ts
@@ -1,11 +1,6 @@
-import { path, slidingWindows } from "./deps.ts";
-const root = path.dirname(
-  Deno.build.os === "windows"
-    ? path.win32.fromFileUrl(import.meta.url)
-    : path.posix.fromFileUrl(import.meta.url),
-);
-const nums = Deno.readTextFileSync(path.join(root, "..", "data", "1.txt"))
-  .split("\n").map((n) => parseInt(n, 10));
+import { slidingWindows } from "./deps.ts";
+import { readData} from "./common.ts";
+const nums = readData("1.txt").split("\n").map((n) => parseInt(n, 10));
 let jumps = 0;
 let last = nums[0];
 for (const num of nums) {
---
+++ deno/common.ts
@@ -0,0 +1,10 @@
+import { path } from "./deps.ts";
+const root = path.dirname(
+  Deno.build.os === "windows"
+    ? path.win32.fromFileUrl(import.meta.url)
+    : path.posix.fromFileUrl(import.meta.url),
+);
+
+export function readData(name: string): string {
+    return Deno.readTextFileSync(path.join(root, "..", "data", name));
+}

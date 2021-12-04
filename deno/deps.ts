e982300b3bfaa54dd0eed3a0c6294b3d0f049685
Benjamin Pollack <benjamin@bitquabit.com>
2021-12-04 11:21:16 -0500

    bump dependency versions

--- deno/deps.ts
+++ deno/deps.ts
@@ -1,6 +1,6 @@
-import * as path from "https://deno.land/std@0.116.0/path/mod.ts";
+import * as path from "https://deno.land/std@0.117.0/path/mod.ts";
 export {
   slidingWindows,
   zip,
-} from "https://deno.land/std@0.116.0/collections/mod.ts";
+} from "https://deno.land/std@0.117.0/collections/mod.ts";
 export { path };
